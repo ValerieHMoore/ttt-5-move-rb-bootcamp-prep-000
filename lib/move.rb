3713e886e81899e2895acf64ce943e0c33ebf784
Valerie Moore <periwinkle17@outlook.com>
2018-05-21 18:34:20 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,7 @@ def input_to_index(user_input)
   puts user_input.to_i - 1
 end
 
-def move(board,input_to_index,"X")
-  "#{board[i]} = X"
+def move(board,input_to_index,user_input)
+  puts 
   display_board
 end
